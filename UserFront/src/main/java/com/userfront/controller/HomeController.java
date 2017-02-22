86da3c44f0bc1f018c529ac3ef32f2dc41be9e19
oleglomako <lom-o@yandex.ru>
2017-02-22 18:05:00 +0100

    signupPost method added into HomeController class

--- UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.userfront.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,4 +30,29 @@ public class HomeController {
 		model.addAttribute("user", user);
 		return "signup";
 	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public void signupPost(@ModelAttribute("user") User user,  Model model) {
+
+//        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+//
+//            if (userService.checkEmailExists(user.getEmail())) {
+//                model.addAttribute("emailExists", true);
+//            }
+//
+//            if (userService.checkUsernameExists(user.getUsername())) {
+//                model.addAttribute("usernameExists", true);
+//            }
+//
+//            return "signup";
+//        } else {
+//        	 Set<UserRole> userRoles = new HashSet<>();
+//             userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+//
+//            userService.createUser(user, userRoles);
+//
+//            return "redirect:/";
+//        }
+    }
+	
 }
