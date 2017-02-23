cdd45cdbf44f53b2a959d3eb14749d2344773030
oleglomako <lom-o@yandex.ru>
2017-02-23 15:27:51 +0100

    added method into interface Userservice

--- UserFront/src/main/java/com/userfront/service/UserService.java
+++ UserFront/src/main/java/com/userfront/service/UserService.java
@@ -1,5 +1,17 @@
 package com.userfront.service;
 
-public interface UserService {
+import com.userfront.domain.User;
 
+public interface UserService {
+	User findByUsername(String username);
+
+	User findByEmail(String email);
+
+	boolean checkUserExists(String username, String email);
+
+	boolean checkUsernameExists(String username);
+
+	boolean checkEmailExists(String email);
+
+	void save(User user);
 }
