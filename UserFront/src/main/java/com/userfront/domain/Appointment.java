e4a42793145aea85cea33af32090fe838d9bd6c5
oleglomako <lom-o@yandex.ru>
2017-02-23 10:22:39 +0100

    annotation added

--- UserFront/src/main/java/com/userfront/domain/Appointment.java
+++ UserFront/src/main/java/com/userfront/domain/Appointment.java
@@ -2,13 +2,21 @@ package com.userfront.domain;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Appointment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
     private Date date;
     private String location;
     private String description;
     private boolean confirmed;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
 	public Long getId() {
