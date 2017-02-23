2828da4b2089ba5bd658d0adfd266a98c942b908
oleglomako <lom-o@yandex.ru>
2017-02-23 10:24:36 +0100

    import added

--- UserFront/src/main/java/com/userfront/domain/Appointment.java
+++ UserFront/src/main/java/com/userfront/domain/Appointment.java
@@ -3,7 +3,11 @@ package com.userfront.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Appointment {
