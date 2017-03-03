df4703fb231993c94de972a3cc650a85a19ef7d6
oleglomako <lom-o@yandex.ru>
2017-03-03 12:38:27 +0100

    uncomment feature transactionService.savePrimaryDepositTransaction(primaryTransaction); and transactionService.saveSavingsDepositTransaction(savingsTransaction);

--- UserFront/src/main/java/com/userfront/service/UserServiceImpl/AccountServiceImpl.java
+++ UserFront/src/main/java/com/userfront/service/UserServiceImpl/AccountServiceImpl.java
@@ -15,6 +15,7 @@ import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.SavingsTransaction;
 import com.userfront.domain.User;
 import com.userfront.service.AccountService;
+import com.userfront.service.TransactionService;
 import com.userfront.service.UserService;
 
 @Service
@@ -30,6 +31,9 @@ public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private TransactionService transactionService;
 
     public PrimaryAccount createPrimaryAccount() {
         PrimaryAccount primaryAccount = new PrimaryAccount();
@@ -51,6 +55,7 @@ public class AccountServiceImpl implements AccountService {
 		return savingsAccountDao.findByAccountNumber(savingsAccount.getAccountNumber());
 	}
 	
+
     public void deposit(String accountType, double amount, Principal principal) {
         User user = userService.findByUsername(principal.getName());
 
@@ -62,7 +67,7 @@ public class AccountServiceImpl implements AccountService {
             Date date = new Date();
 
             PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Deposit to Primary Account", "Account", "Finished", amount, primaryAccount.getAccountBalance(), primaryAccount);
-           // transactionService.savePrimaryDepositTransaction(primaryTransaction);
+            transactionService.savePrimaryDepositTransaction(primaryTransaction);
             
         } else if (accountType.equalsIgnoreCase("Savings")) {
             SavingsAccount savingsAccount = user.getSavingsAccount();
@@ -71,7 +76,7 @@ public class AccountServiceImpl implements AccountService {
 
             Date date = new Date();
             SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Deposit to savings Account", "Account", "Finished", amount, savingsAccount.getAccountBalance(), savingsAccount);
-            //transactionService.saveSavingsDepositTransaction(savingsTransaction);
+            transactionService.saveSavingsDepositTransaction(savingsTransaction);
         }
     }
 
